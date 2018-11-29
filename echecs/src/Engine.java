3e3c7ba047cb112ce5607ca9d87c1fc86f9e88c1
p-Ledoyen <paul.ledoyen289@gmail.com>
2018-11-29 10:55:05 -0500

    Structure de base

---
+++ echecs/src/Engine.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Engine {
+
+    Board board = new Board();
+    Evaluator evaluator = new Evaluator();
+
+
+    public void run() {
+
+        while(true) {
+            Scanner stdin = new Scanner(System.in);
+            String input =  stdin.nextLine();
+
+        }
+
+    }
+}
