900d331da9c57b54bfa17f50d925ce2d00f6c1fd
p-Ledoyen <paul.ledoyen289@gmail.com>
2018-12-07 20:19:19 -0500

    Création du lecteur d'entrées

--- echecs/src/Engine.java
+++ echecs/src/Engine.java
@@ -1,18 +1,20 @@
 import java.util.Scanner;
 
+
 public class Engine {
 
     Board board = new Board();
     Evaluator evaluator = new Evaluator();
 
 
-    public void run() {
-
-        while(true) {
-            Scanner stdin = new Scanner(System.in);
-            String input =  stdin.nextLine();
-
-        }
+    public void run(){
+        //Start input thread
+        Link input = new Link();
+        Thread inputThread = new Thread(input);
+        inputThread.start();
+        while(true);
+        //Stop the input thread
+        //input.stopRunning();
 
     }
 }
